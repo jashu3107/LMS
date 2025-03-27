7364245b54bd9254e1e6caf794c79229e577ff33
Manikanta54331 <manikanta54331@gmail.com>
2025-03-27 15:05:34 +0530

    Added GetLoans API

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const {Users} = require("../models/associations.js");
 const { getLoansController } = require("../controllers/vendorControllers/getLoansControllers.js");
 const { getLoansValidation } = require("../validation/vendorValidation/getLoansValidation.js");
 const router = express.Router();
